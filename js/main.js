da1499cea5c5129488a11bfbeb3f1bb06c1417bf
hasinabegom <105340164+hasinabegom@users.noreply.github.com>
2022-05-14 13:15:37 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,10 +16,7 @@ $(document).ready(function () {
       600
     );
     return false;
-  }
-  
-  );
-
+  });
 
   $(window).scroll(function () {
     if ($(window).scrollTop() > 58) {
@@ -33,3 +30,47 @@ $(document).ready(function () {
     $("header").addClass("sticky");
   }
 });
+
+
+// active nav link
+// const sections = document.querySelectorAll("section");
+// const navLi = document.querySelectorAll("nav ul li");
+// window.onscroll = () => {
+//   var current = "";
+
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop;
+//     console.log(pageXOffset)
+//     if (pageYOffset >= sectionTop - 60) {
+//       current = section.getAttribute("id");
+//     }
+//   });
+
+//   navLi.forEach((li) => {
+//     li.classList.remove("active");
+//     if (li.classList.contains(current)) {
+//       li.classList.add("active");
+//     }
+//   });
+// };
+
+$(window)
+  .scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    // if (scrollDistance >= 850) {
+    // 		$('nav').fadeIn("fast");
+    // } else {
+    // 		$('nav').fadeOut("fast");
+    // }
+
+    // Assign active class to nav links while scolling
+    $("section").each(function (i) {
+      if ($(this).position().top - 100 <= scrollDistance) {
+        $(".navigation li a.active").removeClass("active");
+        $(".navigation li a").eq(i).addClass("active");
+      }
+    });
+  })
+  .scroll();
