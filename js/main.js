06398914c17e544b8feb965e3a3cbdbedd50484b
hasinabegom <105340164+hasinabegom@users.noreply.github.com>
2022-05-16 22:52:25 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
   $('a[href^="#"]').click(function () {
     console.log($('a[href^="#"]'));
     var hash = $(this).attr("href");
+    // console.log($(hash).position().top);
     $("html, body").animate(
       {
         scrollTop: $(hash).offset().top - 64,
@@ -31,26 +32,29 @@ $(document).ready(function () {
   }
 });
 
-
-// Active on scroll
-
+// scroll window
 $(window)
   .scroll(function () {
     var scrollDistance = $(window).scrollTop();
 
     // Show/hide menu on scroll
-    // if (scrollDistance >= 850) {
-    // 		$('nav').fadeIn("fast");
-    // } else {
-    // 		$('nav').fadeOut("fast");
-    // }
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
 
     // Assign active class to nav links while scolling
     $("section").each(function (i) {
-      if ($(this).position().top - 100 <= scrollDistance) {
+      if ($(this).position().top - 300 <= scrollDistance) {
         $(".navigation li a.active").removeClass("active");
         $(".navigation li a").eq(i).addClass("active");
       }
     });
   })
   .scroll();
+
+
+
+
+  
